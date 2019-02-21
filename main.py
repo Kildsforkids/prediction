9af11492491f87e4303a90bbbbc58465a00cc3bb
Kildsforkids <kildsforkids@gmail.com>
2019-02-21 17:57:28 +1000

    Выбор элемента из ListBox

--- main.py
+++ main.py
@@ -3,12 +3,32 @@ import json
 import mysql.connector
 from mysql.connector import errorcode
 
+# Функция записи выбранного значения
+def remember():
+    if len(last_selected) > 0:
+        if last_selected['widget'] == 'spinbox':
+            check.update({last_selected['atr']:spin1.get()})
+    print(check)
 
+# Функция выбора элемента из ListBox'a
 def select(event):
-    print(str(lbox.curselection()[0])+" - "+lbox_data[lbox.curselection()[0]])
+    if len(lbox.curselection()) > 0:
+        last_selected = fields[lbox.curselection()[0]]
+        print(str(lbox.curselection()[0])+" - "+last_selected['atr'])
+        if last_selected['widget'] == 'option':
+            # s1 = StringVar(value=current['values'][0])
+            # option1 = OptionMenu(root, s1, current['values'])
+            # option1.place(x=190, y=28)
+            option1.pack(side=RIGHT)
+            spin1.pack_forget()
+        elif last_selected['widget'] == 'spinbox':
+            # spin1 = Spinbox(root, from_=current['values'][0], to=current['values'][1])
+            # spin1.place(x=193,y=88)
+            spin1.pack(side=RIGHT)
+            option1.pack_forget()
 
-
-def check():
+# Функция расчета "весов"
+def weigh():
     with open("local_db.json", "r") as read_file:
         data = json.load(read_file)
         read_file.close()
@@ -23,12 +43,17 @@ def check():
     print(weights)
 
 
-data = []
-atr = ['id', 'sex', 'age', 'diagnose']
-check = {'sex':'M', 'age':51}
-weights = {'P':0, 'X':0}
-lbox_data = ['Живот','Желчный пузырь(форма)','Размер желчного пузыря(длина)','Размер желчного пузыря(ширина)','Просвет пузыря','Толщина стенок']
-was_connected = False
+last_selected = {}                      # Последний выбранный атрибут
+data = []                               # Массив словарей для работы с локальным хранилищем
+atr = ['id', 'sex', 'age', 'diagnose']  # Список атрибутов
+check = {}                              # Список атрибутов, по которым производится сравнение
+weights = {'P':0, 'X':0}                # Список болезнь - "вес"
+# lbox_data = ['Пол', 'Возраст']
+fields = [                              # Поля в ListBox'e
+    {'field':'Пол', 'atr':'sex', 'widget':'option', 'values':['Мужской','Женский']},
+    {'field':'Возраст', 'atr':'age', 'widget':'spinbox', 'values':[0,10]}
+]
+was_connected = False                   # Удалось соединиться с сервером?
 try:
     connection = mysql.connector.connect(user='root', password='',
                                 host='localhost', database='diagmed', port=3360)
@@ -71,11 +96,18 @@ scrollbar = Scrollbar(root, orient="vertical")
 
 lbox=Listbox(root,width=35,height=15, yscrollcommand=scrollbar.set)
 lbox.place(x=5,y=30)
-for i in lbox_data:
-    lbox.insert(END,i)
+for field in fields:
+    lbox.insert(END, field['field'])
 lbox.bind('<<ListboxSelect>>', select)
 
 scrollbar.config(command=lbox.yview)
 scrollbar.pack(side="right", fill="y")
+
+s1 = StringVar(value='Мужской')
+option1 = OptionMenu(root, s1, 'Мужской', 'Женский')
+spin1 = Spinbox(root, from_=0, to=10, width=5)
+
+btn = Button(root, text="Запомнить", command=remember)
+btn.pack(side=BOTTOM)
 
 root.mainloop()
