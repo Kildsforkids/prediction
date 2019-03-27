f970c91e8823d270d92892219910a21ea6388513
Игорь Сильченко <kildsforkids@gmail.com>
2019-03-27 17:24:46 +1000

    #5

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import translations
 def connect(host, user, password, database, port):
     connection = None
     try:
+        # Пытаемся установить соединение
         connection = mysql.connector.connect(user=user, password=password,
                                              host=host, database=database,
                                              port=port)
@@ -32,42 +33,145 @@ def connect(host, user, password, database, port):
 
 # Функция записи в JSON-файл
 def json_write(file, data):
+    # Открываем файл на запись
     with open(file, "w") as write_file:
+        # Записываем данные
         json.dump(data, write_file)
+        # Закрываем файл
         write_file.close()
 
 
 # Функция считывания из JSON-файла
 def json_read(file):
+    # Открываем файл на чтение
     with open(file, "r") as read_file:
+        # Считываем данные
         data = json.load(read_file)
+        # Закрываем файл
         read_file.close()
+    # Возвращаем данные
     return data
 
 
 # Функция записи данных из БД в JSON-файл
 def load(connection):
+    # Устанавливаем курсор для SQL-запросов
     cursor = connection.cursor()
     # Получаем результат по отправленному SQL-запросу
     cursor.execute("select * from directory;")
     # Получаем список атрибутов
     atr_list = [i[0] for i in cursor.description]
+    # Создаем пустой список для историй болезней
     data = []
     # Перенос записей из БД в data с непустыми атрибутами
     for row in cursor:
+        # Добавляем словарь (для записи - атрибут: значение - из БД)
         data.append({})
+        # Заводим индекс для перехода к атрибуту с непустым значением
         k = 0
+        # Пробегаемся по столбцам и добавляеи записи в словаре -
+        # атрибут: значение
         for col in row:
+            # Если значение есть
             if col is not None:
+                # Обнволяем пары - атрибут: значение -
+                # в последнем словаре списка
                 data[len(data)-1].update({atr_list[k]: col})
+            # Увеличиваем индекс
             k += 1
+    # Закрываем подключение к БД
     connection.close()
+    # Приводим строковые значения к стандартному виду
+    for history in data:
+        for key in history:
+            # Если не из диагноза и пола
+            if key != 'diagnose' and key != 'sex':
+                # Если тип значения - строка
+                if type(history[key]) is str:
+                    # Если строка состоит только из целого числа
+                    if history[key].isdigit():
+                        # Привести строку к целому числу
+                        history[key] = int(history[key])
+                    else:
+                        # Привести строку к нижнему регистру, без пробелов,
+                        # ё = е
+                        history[key] = history[key].lower().replace(' ', ''). \
+                                                        replace('ё', 'е')
     # Запись из data в JSON-файл
     json_write("local_db.json", data)
     # Удаляем ненужные атрибуты из списка
     atr_list.remove('id')
     atr_list.remove('diagnose')
+    # Сохраняем список атрибутов в JSON-файле
     json_write("attributes.json", atr_list)
+
+
+# Функция заполнения списка словарями - {атрибут, виджет, значения}
+def create_fields_list():
+    global fields, atr_list
+
+    # Список атрибутов и их возможных значений
+    fields = []
+    # Получаем список атрибутов
+    atr_list = json_read("attributes.json")
+    # Получаем данные (истории болезней)
+    data = json_read("local_db.json")
+    # Для каждого атрибута
+    for atr in atr_list:
+        # Создаем в списке словарь
+        fields.append({})
+        # Список значений (если строковые, то для выпадающего меню,
+        # если числовые, то для запоминания последнего введенного значения)
+        values = []
+        # По умолчанию виджет - SpinBox (числовое поле)
+        widget_type = 'spinbox'
+        # Для каждой истории болезни
+        for history in data:
+            # Если есть такой атрибут
+            if history.get(atr) is not None:
+                # Если значение атрибута - строка
+                if type(history[atr]) is str:
+                    # Если такой строки еще нет
+                    if history[atr] not in values:
+                        # Добавить строку в список значений
+                        # для выпадающего списка (OptionMenu)
+                        values.append(history[atr])
+                    # Тип виджета - OptionMenu (выпадающий список)
+                    widget_type = 'option'
+                # Если значение атрибута - целое число
+                elif type(history[atr]) is int:
+                    # Если атрибут - не возраст
+                    if atr != 'age':
+                        # Тип виджета - CheckButton (окно с галочкой)
+                        widget_type = 'checkbutton'
+                    break
+                # Если значение атрибута - вещественное число
+                elif type(history[atr]) is float:
+                    # Тип виджета - SpinBox
+                    # (числовое поле для вещественных чисел)
+                    widget_type = 'spinbox_float'
+                    break
+                else:
+                    break
+        # Записать словарь
+        # {'atr': название атрибута,
+        #  'widget': тип виджета,
+        #  'values': значения атрибута
+        # } в список словарей
+        fields[len(fields)-1].update({'atr': atr})
+        fields[len(fields)-1].update({'widget': widget_type})
+        fields[len(fields)-1].update({'values': values})
+
+
+# Проверка на минимальное количество выбранных атрибутов для доступа к анализу
+def check_outputbox():
+    # Если атрибутов >= чем минимальное
+    if outputbox.size() >= weight_min_value:
+        # Сделать активной кнопку "Анализировать"
+        weigh_btn['state'] = 'normal'
+    else:
+        # Сделать неактивной кнопку ""Анализировать
+        weigh_btn['state'] = 'disabled'
 
 
 # Функция обновления списка выбранных атрибутов
@@ -77,9 +181,14 @@ def outputbox_refresh():
     for atr in check:
         # Проверка на наличие перевода названия атрибута
         if translations.get(atr) is not None:
+            # Перевести название атрибута и добавить
+            # в список выбранных атрибтов для сравнения
             outputbox.insert('end', translations[atr]+' - '+str(check[atr]))
         else:
+            # Добавить атрибут без перевода
             outputbox.insert('end', atr+' - '+str(check[atr]))
+    # Проверить на минимальное количество выбранных атрибутов
+    check_outputbox()
 
 
 # Функция записи атрибута в список атрибутов сравнения
@@ -93,11 +202,11 @@ def remember():
             attribute = last_selected['atr']
             # Запомнить значение атрибута из его виджета
             if widget == 'spinbox':
-                str = spin.get()
-                if str.isdigit() and len(str) <= 3:
-                    check.update({attribute: int(str)})
+                string = spin.get()
+                if string.isdigit() and len(string) <= 3:
+                    check.update({attribute: int(string)})
                     last_selected['values'].clear()
-                    last_selected['values'].append(str)
+                    last_selected['values'].append(string)
             elif widget == 'option':
                 check.update({attribute: option_value.get()})
             elif widget == 'checkbutton':
@@ -106,13 +215,28 @@ def remember():
                 })
                 last_selected['values'].clear()
                 last_selected['values'].append(checkbutton_value.get())
+            elif widget == 'spinbox_float':
+                string = spin.get()
+                can = True
+                try:
+                    float(string)
+                except ValueError:
+                    can = False
+                if can:
+                    check.update({attribute: float(string)})
+                    last_selected['values'].clear()
+                    last_selected['values'].append(string)
+            # Обновить список выбранных атрибутов
             outputbox_refresh()
 
 
 # Функция удаления атрибута из списка атрибутов сравнения
 def forget():
+    # Получаем текущий выбранный элемент
     ocs = outputbox.curselection()
+    # Если есть выбранный элемент
     if len(ocs) > 0:
+        # Получаем индекс
         id = lbox.get(0, 'end').index(outputbox.get(ocs).split(' -')[0])
         del check[fields[id]['atr']]
         outputbox_refresh()
@@ -135,7 +259,8 @@ def create_widget():
             )
         option_value.set(last_selected['values'][0])
         option.place(relx=0.5, rely=0.5, anchor='center')
-    elif last_selected['widget'] == 'spinbox':
+    elif last_selected['widget'] == 'spinbox' or \
+            last_selected['widget'] == 'spinbox_float':
         if len(last_selected['values']) > 0:
             spin_value.set(last_selected['values'][0])
         else:
@@ -174,7 +299,7 @@ def select(event):
 
 # Функция анализа
 def weigh():
-    weight_min_value = 5
+    # weight_min_value = 5
     weights = {}
     # Считываем в data данные из JSON-файл
     data = json_read("local_db.json")
@@ -214,7 +339,7 @@ def weigh():
 # Функция создания интерфейса
 def main():
     global lbox, outputbox, spin, spin_value, option, option_value, window_width, window_height, \
-        checkbutton_value, checkbutton, root
+        checkbutton_value, checkbutton, root, weigh_btn
 
     window_width = 800
     window_height = 600
@@ -224,6 +349,8 @@ def main():
     root.title('Мед-Анализатор 3000')
     root.geometry(str(window_width)+'x'+str(window_height))
     root.minsize(window_width, window_height)
+    root.maxsize(window_width, window_height)
+    root.bind('<Return>', lambda event: remember())
 
     frame = Frame(root)
     frame2 = Frame(root)
@@ -255,12 +382,11 @@ def main():
     option_value = StringVar(value=fields[0]['values'][0])
     option = OptionMenu(root, option_value, *fields[0]['values'])
     spin_value = StringVar(value='0')
-    spin = Spinbox(root, from_=0, to=200, textvariable=spin_value, width=5)
+    spin = Spinbox(root, from_=0, to=200, textvariable=spin_value, width=5,
+                   format="%.2f")
     checkbutton_value = IntVar()
     checkbutton = Checkbutton(text="Есть", variable=checkbutton_value,
                               onvalue=1, offvalue=0)
-    root.bind('<Return>', lambda event: remember())
-    root.bind('<space>', lambda event: weigh())
 
     btn = Button(root, text="Запомнить", command=remember)
     btn.pack()
@@ -271,53 +397,29 @@ def main():
     btn = Button(root, text="Удалить все", command=forget_all)
     btn.pack()
 
-    btn = Button(root, text="Анализировать", command=weigh)
-    btn.pack(side='bottom')
+    weigh_btn = Button(root, text="Анализировать", state='disabled',
+                       padx=5, pady=5, command=weigh)
+    weigh_btn.pack(side='bottom')
 
     root.mainloop()
 
 
 if __name__ == '__main__':
-    global check, fields, translations, atr_list
+    global check, fields, translations, atr_list, weight_min_value
 
     # Словарь для перевода
     translations = translations.get_translation('ru')
-    # Список атрибутов и их возможных значений
-    fields = []
     # Словарь атрибутов сравнения
     check = {}
-    atr_list = []
-
+    # Минимальное необходимое число для критериев Max и People
+    weight_min_value = 5
+    # Попытка подключения к БД
     connection = connect('62.249.154.246', 'medic', 'medlab', 'diagmed', 33547)
     # Если удалось подключиться
     if connection is not None:
         # Загрузить данные в JSON-файл
         load(connection)
     # Заполнение fields атрибутами и соответстующими им значениями и виджетами
-    atr_list = json_read("attributes.json")
-    data = json_read("local_db.json")
-    for atr in atr_list:
-        fields.append({})
-        values = []
-        widget_type = 'spinbox'
-        for history in data:
-            if history.get(atr) is not None:
-                if type(history[atr]) is str:
-                    if history[atr] not in values:
-                        values.append(history[atr])
-                elif type(history[atr]) is int:
-                    if atr != 'age':
-                        widget_type = 'checkbutton'
-                    break
-                elif type(history[atr]) is float:
-                    # print(atr, history[atr])
-                    break
-                else:
-                    break
-        if len(values) > 0:
-            widget_type = 'option'
-        fields[len(fields)-1].update({'atr': atr})
-        fields[len(fields)-1].update({'widget': widget_type})
-        fields[len(fields)-1].update({'values': values})
-
+    create_fields_list()
+    # Запуск интерфейса
     main()
